77648db781a028622292e02dba81b051ef2be285
wilson <wilsnyeh@gmail.com>
2023-02-06 15:13:35 -0800

    first checkpoint

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pet = (props) => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, props.name),
+        React.createElement('h2', {}, props.animal),
+        React.createElement('h2', {}, props.breed),
+    ])
+}
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement('h1', {}, 'some text'),
+            React.createElement(Pet, {
+                name: 'Combo',
+                animal: 'Dog',
+                breed: 'Yorkie-mix'
+            }),
+            React.createElement(Pet),
+            React.createElement(Pet),
+        ]
+    )
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
