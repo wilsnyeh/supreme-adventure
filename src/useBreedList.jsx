84dedcb1e6126f938c449070db38cd7dcf9bdd2b
wilson <wilsnyeh@gmail.com>
2023-03-12 23:13:59 -0700

    usebreedlist

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -22,6 +22,10 @@ export default function useBreedList(animal) {
             const res = await fetch(
                 `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
             )
+            const json = await res.json();
+            localCache[animal] = json.breeds || []
+            setBreedList(localCache[animal]);
+            setStatus("loaded")
         }
     })
 }
