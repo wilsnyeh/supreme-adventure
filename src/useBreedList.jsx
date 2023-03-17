d886641462a82b1c56f23cb7df6236f245015798
wilson <wilsnyeh@gmail.com>
2023-03-17 14:26:05 -0700

    refactor breedlist

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -1,32 +1,37 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
-const localCache = {};
+// const localCache = {};
 
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState('unloaded');
+  const results = useQuery(["breeds", animal], fetchBreedList);
+  // const [breedList, setBreedList] = useState([]);
+  // const [status, setStatus] = useState('unloaded');
 
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal])
-        } else {
-            requestBreedList();
-        }
+  // useEffect(() => {
+  //     if (!animal) {
+  //         setBreedList([]);
+  //     } else if (localCache[animal]) {
+  //         setBreedList(localCache[animal])
+  //     } else {
+  //         requestBreedList();
+  //     }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus('loading');
+  //     async function requestBreedList() {
+  //         setBreedList([]);
+  //         setStatus('loading');
 
-            const res = await fetch(
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            )
-            const json = await res.json();
-            localCache[animal] = json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-    }, [animal]);
-    return [breedList, status];
+  //         const res = await fetch(
+  //             `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  //         )
+  //         const json = await res.json();
+  //         localCache[animal] = json.breeds || [];
+  //         setBreedList(localCache[animal]);
+  //         setStatus("loaded");
+  //     }
+  // }, [animal]);
+
+  // the .? if results is available - ?? is ternary, where if results/data fails, return empty array
+  return [results?.data?.breeds ?? [], results.status];
 }
