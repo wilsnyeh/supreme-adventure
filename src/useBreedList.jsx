f3769a2e5e56107897a3319aeb3ca8d3374eec21
wilson <wilsnyeh@gmail.com>
2023-03-17 14:28:18 -0700

    refactor breedlist

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -6,32 +6,33 @@ import fetchBreedList from "./fetchBreedList";
 
 export default function useBreedList(animal) {
   const results = useQuery(["breeds", animal], fetchBreedList);
-  // const [breedList, setBreedList] = useState([]);
-  // const [status, setStatus] = useState('unloaded');
-
-  // useEffect(() => {
-  //     if (!animal) {
-  //         setBreedList([]);
-  //     } else if (localCache[animal]) {
-  //         setBreedList(localCache[animal])
-  //     } else {
-  //         requestBreedList();
-  //     }
-
-  //     async function requestBreedList() {
-  //         setBreedList([]);
-  //         setStatus('loading');
-
-  //         const res = await fetch(
-  //             `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-  //         )
-  //         const json = await res.json();
-  //         localCache[animal] = json.breeds || [];
-  //         setBreedList(localCache[animal]);
-  //         setStatus("loaded");
-  //     }
-  // }, [animal]);
 
   // the .? if results is available - ?? is ternary, where if results/data fails, return empty array
   return [results?.data?.breeds ?? [], results.status];
 }
+
+// const [breedList, setBreedList] = useState([]);
+// const [status, setStatus] = useState('unloaded');
+
+// useEffect(() => {
+//     if (!animal) {
+//         setBreedList([]);
+//     } else if (localCache[animal]) {
+//         setBreedList(localCache[animal])
+//     } else {
+//         requestBreedList();
+//     }
+
+//     async function requestBreedList() {
+//         setBreedList([]);
+//         setStatus('loading');
+
+//         const res = await fetch(
+//             `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+//         )
+//         const json = await res.json();
+//         localCache[animal] = json.breeds || [];
+//         setBreedList(localCache[animal]);
+//         setStatus("loaded");
+//     }
+// }, [animal]);
