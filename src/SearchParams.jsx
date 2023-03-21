a6809546b018e8b13270a4c68563c65cc431db71
wilson <wilsnyeh@gmail.com>
2023-03-21 15:57:18 -0700

    adopt pet context complete

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
 import fetchSearch from "./fetchSearch";
@@ -17,6 +18,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   //   const [breed, setBreed] = useState("");
   const [breeds] = useBreedList(animal);
+  const [adoptedPet] = useContext(AdoptedPetContext);
 
   //   useEffect(() => {
   //     requestPets();
@@ -48,6 +50,11 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
           <input
