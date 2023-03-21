2638c22e8b7ffc26a01cbfb43e99c96dc7363b2d
wilson <wilsnyeh@gmail.com>
2023-03-21 14:28:57 -0700

    adopted pet context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import AdoptedPetContext from "./AdoptedPetContext";
+import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 // const App = () => {
@@ -37,16 +38,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const AdoptedPetContext = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/"> Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/"> Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
